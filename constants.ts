9023d19cf0fce03224960ee957efa680a32504e2
Shoubhik <64132039+shoubhik-raj@users.noreply.github.com>
2025-03-13 04:23:35 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://virtual-lab-p2-backend.onrender.com/api";
-export const SERVER_URL = "http://virtual-lab-p2-backend.onrender.com";
+export const API_URL = "https://virtual-lab-p2-backend.onrender.com/api";
+export const SERVER_URL = "https://virtual-lab-p2-backend.onrender.com";
